fd0f2de1545705972ce8428b04c7a4d39bc04a17
RahatAlam <webdevrahatalam@gmail.com>
2023-01-20 20:43:37 +0600

    Button Updated

--- lib/main.dart
+++ lib/main.dart
@@ -6,30 +6,39 @@ import 'package:flutter/material.dart'; // Libary File
 /// home: , appBar: , title: etc are called 'property / attributes'
 ///
 
-main() { // projects entry point
-  runApp(HelloWorldApp()); //HelloWorldApp is an application which is put on the runApp widget.
+main() {
+  // projects entry point
+  runApp(
+      HelloWorldApp()); //HelloWorldApp is an application which is put on the runApp widget.
 }
 
-class HelloWorldApp extends StatelessWidget { //HelloWorldApp class becomes a widget after using extends means it inherts from statelesswidget
+class HelloWorldApp extends StatelessWidget {
+  //HelloWorldApp class becomes a widget after using extends means it inherts from statelesswidget
   @override
   Widget build(BuildContext context) {
-      return MaterialApp( //MaterialApp is a predifine class of flutter which indicates to set the apps ui / apps theme. Its also a core component of Flutter
+    return MaterialApp( //MaterialApp is a predifine class of flutter which indicates to set the apps ui / apps theme. Its also a core component of Flutter
       /// property/attributes : home,appbar
       // Scaffold is a class in flutter which provides many widgets
 
       // These are the settings of MaterialApp, by which we control the application
-      debugShowCheckedModeBanner: false, //removes the debugshowcheckmodeBanner
-      theme: ThemeData(primarySwatch: Colors.amber), // sets the color of theme
-      darkTheme: ThemeData(primarySwatch: Colors.brown), // sets the color of the darkTheme
-      color: Colors.green, // sets the primary color
+        debugShowCheckedModeBanner: false,
+        //removes the debugshowcheckmodeBanner
+        theme: ThemeData(primarySwatch: Colors.amber),
+        // sets the color of theme
+        darkTheme: ThemeData(primarySwatch: Colors.brown),
+        // sets the color of the darkTheme
+        color: Colors.green,
+        // sets the primary color
 
-      home: homeactivity()
+        home: homeactivity()
     );
   }
 }
+
 class homeactivity extends StatelessWidget {
   const homeactivity({Key? key}) : super(key: key);
-  MySnackbar(message, context){
+
+  MySnackbar(message, context) {
     return ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text(message))
     );
@@ -38,90 +47,51 @@ class homeactivity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('RahatAlam'),
-        titleSpacing: 50,
-        centerTitle: true,
-        toolbarHeight: 70,
-        toolbarOpacity: 1,
-        elevation: 0,
-        backgroundColor: Colors.green,
-        
-        actions: [
-          IconButton(onPressed: (){MySnackbar("Account", context);}, icon: Icon(Icons.account_circle_sharp)),
-          IconButton(onPressed: (){MySnackbar("Alarm", context);}, icon: Icon(Icons.access_alarm_rounded)),
-          IconButton(onPressed: (){MySnackbar("Comment", context);}, icon: Icon(Icons.comment))
-        ],
-      ),
+        appBar: AppBar(
+          title: Text('RahatAlam'),
+          titleSpacing: 50,
+          centerTitle: true,
+          toolbarHeight: 70,
+          toolbarOpacity: 1,
+          elevation: 0,
+          backgroundColor: Colors.green,
 
-      body: SingleChildScrollView( //SingleChildScrollView is used for scrolling the screen
-       child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            height: 150,
-            width: 150,
-              //padding: EdgeInsets.fromLTRB(left, top, right, bottom),
-             //padding: EdgeInsets.all(20),
-           margin: EdgeInsets.all(20),
-            alignment: Alignment.center,
-            //child: Text('RahatAlam'),
-            decoration: BoxDecoration(
-                color: Colors.green,
-                border: Border.all(color: Colors.amber, width: 6),
-            ),
-            child: Image.network('https://images.pexels.com/photos/918327/pexels-photo-918327.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
-          ),
-          Container(
-            height: 150,
-            width: 150,
-            //padding: EdgeInsets.fromLTRB(left, top, right, bottom),
-            //padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(20),
-            alignment: Alignment.center,
-            //child: Text('RahatAlam'),
-            decoration: BoxDecoration(
-              color: Colors.green,
-              border: Border.all(color: Colors.amber, width: 6),
-            ),
-            child: Image.network('https://images.pexels.com/photos/918327/pexels-photo-918327.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
-          ),
-          Column(
-            //crossAxisAlignment: CrossAxisAlignment.end,
+          actions: [
+            IconButton(onPressed: () {
+              MySnackbar("Account", context);
+            }, icon: Icon(Icons.account_circle_sharp)),
+            IconButton(onPressed: () {
+              MySnackbar("Alarm", context);
+            }, icon: Icon(Icons.access_alarm_rounded)),
+            IconButton(onPressed: () {
+              MySnackbar("Comment", context);
+            }, icon: Icon(Icons.comment))
+          ],
+        ),
+
+        body: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Container(
-                height: 200,
-                width: 200,
-                //padding: EdgeInsets.fromLTRB(left, top, right, bottom),
-                padding: EdgeInsets.all(20),
-                margin: EdgeInsets.all(20),
-                alignment: Alignment.center,
-                //child: Text('RahatAlam'),
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  border: Border.all(color: Colors.amber, width: 6),
-                ),
-                child: Image.network('https://images.pexels.com/photos/918327/pexels-photo-918327.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
-              ),
-              Container(
-                height: 200,
-                width: 200,
-                //padding: EdgeInsets.fromLTRB(left, top, right, bottom),
-                padding: EdgeInsets.all(20),
-                margin: EdgeInsets.all(20),
-                alignment: Alignment.center,
-                //child: Text('RahatAlam'),
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  border: Border.all(color: Colors.amber, width: 6),
-                ),
-                child: Image.network('https://images.pexels.com/photos/918327/pexels-photo-918327.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
-              )
-            ],
+
+            TextButton(onPressed: ()
+        {MySnackbar('Iam Text Button', context);},
+        child: Text('Text Button')),
+
+
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(  // it is used inside the Elevated Button, to style it
+          backgroundColor: Colors.green,
+          foregroundColor: Colors.amber,
+          textStyle: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20)
           ),
-        ],
-      ),
-      ),
+          padding: EdgeInsets.all(5),
+          ),
+        onPressed: (){}, child: Text('Click Here')
+        ),
+            ],
+      )
     );
   }
 }
