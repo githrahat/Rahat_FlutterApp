0c9b7c0186da95556db0f2e186e55931195be3f6
RahatAlam <webdevrahatalam@gmail.com>
2023-01-25 19:12:22 +0600

    SimpleForm_AlertDialog_Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,32 @@
-import 'package:flutter/material.dart'; // Libary File
+import 'package:flutter/material.dart';
 
-/// main() -> Entry points of code
-/// runApp() -> Entry points of Apps
-/// MaterialApp, Scaffold, AppBar, Text etc are 'class / Widgets'
-/// home: , appBar: , title: etc are called 'property / attributes'
-///
-
-main() { // projects entry point
-  runApp(HelloWorldApp()); //HelloWorldApp is an application which is put on the runApp widget.
+main() {
+  runApp(const MyApp());
 }
 
-class HelloWorldApp extends StatelessWidget { //HelloWorldApp class becomes a widget after using extends means it inherts from statelesswidget
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( //MaterialApp is a predifine class of flutter which indicates to set the apps ui / apps theme. Its also a core component of Flutter
-      /// property/attributes : home,appbar
-      // Scaffold is a class in flutter which provides many widgets
-
-      //==================================
-      // These are the settings of MaterialApp, by which we control the application
-      debugShowCheckedModeBanner: false, //removes the debugshowcheckmodeBanner
-      theme: ThemeData(primarySwatch: Colors.green), // sets the color of theme
-      darkTheme: ThemeData(primarySwatch: Colors.green), // sets the color of the darkTheme
-      color: Colors.green, // sets the primary color
-      //=========================================
-
-      home: homeactivity()
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AppsHome(),
     );
-
   }
 }
-class homeactivity extends StatelessWidget {
-  const homeactivity({Key? key}) : super(key: key);
 
+class AppsHome extends StatelessWidget {
+  AppsHome({Key? key}) : super(key: key);
 
-  MySnackbar(message, context){
+  TextEditingController controllerOne = TextEditingController();
+  TextEditingController controllerTwo = TextEditingController();
+  TextEditingController controllerThree = TextEditingController();
+  TextEditingController controllerFour = TextEditingController();
+  TextEditingController controllerFive = TextEditingController();
+
+  /// SnackBar message code
+  SnackBarMessage(message, context) {
     return ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text(message))
     );
@@ -43,55 +35,116 @@ class homeactivity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
+      /// AppBar
       appBar: AppBar(
-        title: Text('RahatAlam'),
-        titleSpacing: 50,
+
+        /// appBar
+        title: Text("Mess App"),
+        //titleSpacing: 15,
         centerTitle: true,
-        toolbarHeight: 70,
         toolbarOpacity: 1,
-        elevation: 0,
-        backgroundColor: Colors.green,
+        toolbarHeight: 85,
+        elevation: 25,
+        backgroundColor: Colors.blueAccent,
 
-
+        /// set action icon in appbar
         actions: [
-          IconButton(onPressed: (){MySnackbar("Account", context);}, icon: Icon(Icons.account_circle_sharp)),
-          IconButton(onPressed: (){MySnackbar("Alarm", context);}, icon: Icon(Icons.access_alarm_rounded)),
-          IconButton(onPressed: (){MySnackbar("Comment", context);}, icon: Icon(Icons.comment))
+          IconButton(
+            onPressed: () {
+              SnackBarMessage('Search', context);
+            },
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+              onPressed: () {
+                SnackBarMessage('Call', context);
+              },
+              icon: Icon(Icons.add_call)),
+          IconButton(
+              onPressed: () {
+                SnackBarMessage('Settings', context);
+              },
+              icon: Icon(Icons.settings_applications))
         ],
       ),
-      body: Center(
-        child: Text('RahatAlam'),
-      ),
 
-     floatingActionButton: FloatingActionButton(
-       elevation: 20,
-       backgroundColor: Colors.green,
-       child: Icon(Icons.arrow_back_ios),
-       onPressed: (){ 
-         MySnackbar('Coming Soon', context);
-       },
-     ),
+      /// Body
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        //crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
 
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home', backgroundColor: Colors.green),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search', backgroundColor: Colors.green),
-          BottomNavigationBarItem(icon: Icon(Icons.perm_contact_calendar_sharp), label: 'Contact', backgroundColor: Colors.green),
+          /// Simple form
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              controller: controllerOne,
+              onChanged: (value){
+                //print(value);
+              },
+              onSubmitted: (value){
+                //print(value);
+              },
+              onTap: (){
+                //SnackBarMessage('Pressed', context);
+              },
+              decoration: InputDecoration(
+                hintText: 'Enter your name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            //margin: EdgeInsets.all(10),
+            //color: Colors.red,
+            child: TextField(
+              controller: controllerTwo,
+              onChanged: (value){
+                //print(value);
+              },
+              onSubmitted: (value){
+                //print(value);
+              },
+              onTap: (){
+                //SnackBarMessage('Pressed', context);
+              },
+              decoration: InputDecoration(
+                hintText: 'Enter your age',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.teal,
+                  foregroundColor: Colors.black87,
+                  elevation: 8,
+                  textStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  )),
+              onPressed: () {
+                print(controllerOne.text);
+                print(controllerTwo.text);
+                //controllerOne.text = 'hassan';
+                showDialog(context: context, builder: (context){
+                  return AlertDialog(
+                    title: Text('Confirmation'),
+                    content: Text('Are you sure for submission?'),
+                    actions: [
+                      TextButton(onPressed: (){}, child: Text('No')),
+                      TextButton(onPressed: (){}, child: Text('Yes')),
+                    ],
+                  );
+                });
+              },
+              child: Text('Sign in')),
         ],
 
-        onTap: (int index){
-          if(index==0){
-            MySnackbar('Home: Work going on', context);
-          }
-          else if(index==1){
-            MySnackbar('Search: Work going on', context);
-          }
-          else if(index==2){
-            MySnackbar('Contact: Work going on', context);
-          }
-        }
       ),
+
+
     );
   }
 }
