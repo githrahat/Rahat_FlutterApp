ae32b394ddd54a91ed51c4f64dc59f2f70bd85e7
RahatZzzz <webdevrahatalam@gmail.com>
2023-04-28 11:23:15 +0600

    Football-Livescore_DONE

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_project/match_list_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,90 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const BookListScreen(),
+      home: const MatchListScreen(),
     );
   }
-}
-
-class BookListScreen extends StatefulWidget {
-  const BookListScreen({Key? key}) : super(key: key);
-
-  @override
-  State<BookListScreen> createState() => _BookListScreenState();
-}
-
-class _BookListScreenState extends State<BookListScreen> {
-
-  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance; /// --> Collection of Database
-  List<Book> Books = [];
-  bool inProgress = false;
-
-  @override
-  void initState() {
-    super.initState();
-    //getBookList();
-  }
-
-  Future<void> getBookList() async {
-    inProgress = true;
-    setState(() {});
-    Books.clear();
-    await firebaseFirestore.collection('Books').get().then((documents) { /// access the collection from firebaseFirestore }
-      for (var doc in documents.docs) {
-        Books.add(Book(doc.get('Name'), doc.get('Writer'), doc.get('Year')));
-      }
-    });
-
-    inProgress = false;
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Books Collection'),
-      ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: firebaseFirestore.collection('Books').snapshots(),
-    builder: (context, snapshot) {
-      if (snapshot.connectionState == ConnectionState.waiting) {
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      }
-      if (snapshot.hasError) {
-        return Center(
-          child: Text(snapshot.error.toString()),
-        );
-      }
-
-      if (snapshot.hasData) {
-        Books.clear();
-        for (var doc in snapshot.data!.docs) {
-          Books.add(Book(doc.get('Name'), doc.get('Writer'), doc.get('Year')));
-        }
-        return ListView.builder(
-            itemCount: Books.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(Books[index].name),
-                subtitle: Text(Books[index].authorName),
-                trailing: Text(Books[index].year),
-              );
-            });
-      }
-      else {
-      return const Center(
-      child: Text('No data available'),
-      );
-      }
-    }),
-    );
-  }
-}
-
-class Book {
-  final String name, authorName, year;
-  Book(this.name, this.authorName, this.year);
 }
