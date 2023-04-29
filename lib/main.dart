6dc5b3b4790f1109fc13f79a838bed6296c5802f
RahatZzzz <webdevrahatalam@gmail.com>
2023-04-29 10:44:10 +0600

    Geolocator-done

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,83 @@
-
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase_project/match_list_screen.dart';
+import 'package:geolocator/geolocator.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+void main(){
+  runApp(const MapApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MapApp extends StatelessWidget {
+  const MapApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MatchListScreen(),
+    return const MaterialApp(
+      home: MapViewScreen(),
     );
   }
 }
+
+class MapViewScreen extends StatefulWidget {
+  const MapViewScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MapViewScreen> createState() => _MapViewScreenState();
+}
+
+class _MapViewScreenState extends State<MapViewScreen> {
+
+   Position? currentLocation;
+
+   void listenToLatestLocation(){
+     Geolocator.getPositionStream().listen((event) {
+       print(event);
+     });
+   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MapView'),
+      ),
+
+      body: Center(
+        child: Text(currentLocation.toString()),
+      ),
+        floatingActionButton: FloatingActionButton(
+        onPressed: () async{
+
+          //location Permission
+          LocationPermission permission = await Geolocator.requestPermission();
+
+          if(permission == LocationPermission.always || permission == LocationPermission.whileInUse) {
+            //current location fetch
+            currentLocation = await Geolocator.getCurrentPosition();
+            print(currentLocation);
+            setState(() {});
+          }
+          else{
+            print("Permission not allowed");
+          }
+    },
+    child: const Icon(Icons.location_on),
+    ),
+
+
+
+
+
+      // body: Center(
+      //   child: SizedBox(
+      //     width: 400,
+      //     height: 500,
+      //     child: GoogleMap(
+      //       initialCameraPosition: CameraPosition(
+      //         target: LatLng(23.793895737073488, 90.40448580672911)
+      //       ),
+      //     ),
+      //   ),
+      // ),
+    );
+  }
+}
+
